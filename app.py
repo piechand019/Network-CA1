923b84a00396c86bc3c9c94ac4544f8488c8855e
piechand019 <piechand19@gmail.com>
2024-11-29 13:25:21 +0000

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def home():
                 <title>CI/CD</title>
                 </head>
                 <body>
-                <h1> Hello World, We have successfully completed the task. From Chandni, Avi and Himanshu. </h1>
+                <h1> Hello World We have successfully completed the task. From Chandni, Avi and Himanshu. </h1>
                 </body>
                 </html>'''
 
